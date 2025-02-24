14eb7afdc443eaf5f3fcaf90f699fd7d8b25d226
Vittorio Picone <36678730+VikSn0w@users.noreply.github.com>
2025-02-25 00:54:50 +0100

    Add files via upload

---
+++ ohvale/data.js
@@ -0,0 +1,117 @@
+const pilots = [{
+      "ID": 1,
+      "name": "Vittorio",
+      "surname": "Picone",
+      "born_year": 2002,
+      "ig_nickname": "vik.desmo",
+      "alias": "Captain_Slow",
+      "weight": 97,
+      "height": 180,
+	  "color": {
+		  "red": 203,
+		  "green": 21,
+		  "blue": 23,
+	  }
+    },{
+      "ID": 2,
+      "name": "Michele",
+      "surname": "De Micco",
+      "born_year": 1993,
+      "ig_nickname": "",
+      "alias": "",
+      "weight": 0,
+      "height": 0,
+	  "color": {
+		  "red": 255,
+		  "green": 102,
+		  "blue": 0,
+	  }
+    },
+	{
+      "ID": 3,
+      "name": "Emanuele",
+      "surname": "Nappi",
+      "born_year": 2003,
+      "ig_nickname": "",
+      "alias": "",
+      "weight": 0,
+      "height": 0,
+	  "color": {
+		  "red": 55,
+		  "green": 55,
+		  "blue": 55,
+	  }
+    },
+	{
+      "ID": 4,
+      "name": "Antonio",
+      "surname": "Chiacchio",
+      "born_year": 2000,
+      "ig_nickname": "",
+      "alias": "",
+      "weight": 0,
+      "height": 0,
+	  "color": {
+		  "red": 34,
+		  "green": 54,
+		  "blue": 125,
+	  }
+    }
+	];
+
+    const times = [{
+      "title": "Uscita 1", 
+      "date": "2025-01-25",
+      "turni": [
+        {
+          "times": [{
+            "pilot_id": 1,
+            "best_time": 99.405,
+            "max_speed": 46.170
+          },{"pilot_id": 2,
+            "best_time": 75.565,
+            "max_speed": 60.981
+          },{"pilot_id": 3,
+            "best_time": 80.421,
+            "max_speed": 57.298
+          },
+		  {"pilot_id": 4,
+            "best_time": 80.330,
+            "max_speed": 57.363
+          }
+		  ]
+        },
+		{
+          "times": [{
+            "pilot_id": 1,
+            "best_time": 87.655,
+            "max_speed": 52.570
+          },{"pilot_id": 2,
+            "best_time": 72.049,
+            "max_speed": 63.956
+          },{"pilot_id": 3,
+            "best_time": 77.536,
+            "max_speed": 59.4320
+          },{"pilot_id": 4,
+            "best_time": 73.257,
+            "max_speed": 62.902
+          }]
+        },
+		{
+          "times": [{
+            "pilot_id": 1,
+            "best_time": 81.029,
+            "max_speed": 59.295
+          },{"pilot_id": 2,
+            "best_time": 71.389,
+            "max_speed": 65.155
+          },{"pilot_id": 3,
+            "best_time": 77.713,
+            "max_speed": 61.082
+          },{"pilot_id": 4,
+            "best_time": 75.440,
+            "max_speed": 64.548
+          }]
+        }
+      ]
+    }];
